3a2c30eebe25a3f37954bfdc93ba392a977e62ab
tshraboni <tanhaislam6886@gmail.com>
2026-04-27 15:06:31 +0600

    abc

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+
 export default function Index() {
   return (
-    <View style={{flex: 1, backgroundColor: "plum"}}>
-      <View style={{width: 200, height: 200, backgroundColor: "green", padding: 50}}>Tanha</View>
-       <View style={{width: 200, height: 200, backgroundColor: "red", padding: 50}}>Shraboni</View>
-       <Text>
-        
-       </Text>
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View style={{ width: 200, height: 200, backgroundColor: "green", padding: 50 }}>
+        <Text>Tanha</Text>
+      </View>
+      <View style={{ width: 200, height: 200, backgroundColor: "red", padding: 50 }}>
+        <Text>Shraboni</Text>
+      </View>
+      <Text>
+        <Text style={{ color: "white"}}>Helloo</Text>
+      </Text>
     </View>
   );
 }
+
