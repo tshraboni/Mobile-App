0a31fdf3fd7c4303554ac424f88b8b86b909858c
tshraboni <tanhaislam6886@gmail.com>
2026-04-27 15:52:35 +0600

    added function

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,25 @@
-import { Text, View } from "react-native";
-
+import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
+const logoImg=require('@/assets/images/emoji3.png')
+const logoImg1=require('@/assets/images/emoji5.png')
 export default function Index() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View style={{ width: 200, height: 200, backgroundColor: "green", padding: 50 }}>
+    <ScrollView style={{ flex: 1}}>
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <View style={{ width: 300, height: 300, backgroundColor: "green", padding: 50 }}>
         <Text>Tanha</Text>
       </View>
-      <View style={{ width: 200, height: 200, backgroundColor: "red", padding: 50 }}>
+      <View style={{ width: 400, height: 400, backgroundColor: "red", padding: 50 }}>
         <Text>Shraboni</Text>
       </View>
       <Text>
-        <Text style={{ color: "white"}}>Helloo</Text>
+        <ImageBackground source={logoImg1} style={{flex:1, padding:50}}/>
+        <Text style={{ color: "white", fontSize:350}}>Helloo</Text>
       </Text>
+      <Image source={logoImg} style={{width: 500, height: 500}}/>
+      <Image source={{uri:"https://picsum.photos/200"}} style={{width: 500, height: 500}}/>
+      
     </View>
+    </ScrollView>
   );
 }
 
